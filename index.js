d98e257089ca4bfd58d4fd3eb96ce1d42d666d17
sakib1416 <sakib1416@gmail.com>
2021-06-10 14:15:21 +0200

    seeking added

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const fileUpload = require('express-fileupload');
 
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false }));
+app.use(bodyParser.json({limit: '50mb'}));
 
 //file upload images for admins
 app.use(express.static('donations'));
@@ -22,7 +22,8 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
   const donorCollection = client.db(`${process.env.DB_NAME}`).collection("donor");
   const donationsCollection = client.db(`${process.env.DB_NAME}`).collection("donations");
-  
+  const usersCollection = client.db(`${process.env.DB_NAME}`).collection("users");
+  const seekingCollection = client.db(`${process.env.DB_NAME}`).collection("seeks");
   //showing all the donations
   app.get("/donations", (req,res) => {
     donationsCollection.find()
@@ -46,8 +47,12 @@ client.connect(err => {
     const location = req.body.location;
     const categories = req.body.categories;
     const delivery = req.body.delivery; 
+    const donorName = req.body.donorName; 
+    const donorEmail = req.body.donorEmail; 
     const img = file.name;
-    const donation = {name, description, location, categories, delivery, img};
+    
+    const donation = {name, description, location, categories, delivery, img, donorName, donorEmail};
+    console.log(donation)
     file.mv(`${__dirname}/donations/${img}`, (err) =>{
       if(err) {
         console.log(err);
@@ -62,7 +67,74 @@ client.connect(err => {
     })
   });
 
+  app.post("/addUser", (req,res) => {
+    const user = {name: req.body.name, email: req.body.email};
+    usersCollection.insertOne(user)
+    .then(result => {
+      console.log(result);
+      res.send(result.insertedCount > 0);
+    })
+  });
 
+  //updating a donation
+  app.patch("/update/donation/:id", (req,res) => {
+    console.log(req.body);
+    const id = ObjectID(req.params.id);
+    //be careful with the $set
+    donationsCollection.updateOne({_id: id}, {
+        $set: {name: req.body.name, description: req.body.description, location: req.body.location, categories: req.body.categories, delivery: req.body.delivery}
+    })
+    .then(result => res.send(result))
+  });
+
+  app.delete("/delete/donation/:id", (req,res) => {
+    const id = ObjectID(req.params.id);
+    donationsCollection.deleteOne({_id: id})
+    .then(documents => res.send(!!documents.value))
+  })
+
+  //adding a seeking post
+  app.post("/addSeek", (req,res) => {
+    const file = req.files.file;
+    const name = req.body.name;
+    const description = req.body.description;
+    const location = req.body.location;
+    const categories = req.body.categories;
+    const delivery = req.body.delivery; 
+    const seekerName = req.body.seekerName; 
+    const seekerEmail = req.body.seekerEmail; 
+    const img = file.name;
+    
+    const seek = {name, description, location, categories, delivery, img, seekerName, seekerEmail};
+    console.log(seek);
+    file.mv(`${__dirname}/seeks/${img}`, (err) =>{
+      if(err) {
+        console.log(err);
+        return res.status(500).send({msg: "failed to upload the image"})
+      }
+      return res.send({name: img, path: `/${img}`})
+    })
+    seekingCollection.insertOne(seek)
+    .then(result => {
+      console.log(result);
+      res.send(result.insertedCount > 0)
+    })
+  });
+
+  //showing all the seeking post
+  app.get("/seekings", (req,res) => {
+    seekingCollection.find()
+    .toArray((err, documents) => {
+      res.send(documents);
+    })
+  });
+
+  //showing a single seeking post
+  app.get("/seeking/:id", (req,res) => {
+    const id = ObjectID(req.params.id);
+    seekingCollection.findOne({_id: id})
+    .then(result => res.send(result));
+  });
 
   console.log("Database connected!");
 });
